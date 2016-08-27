5595e68f3b133aab150bccff8e2028b5b7d429a2
jonathan brenner <jonathan brenner>
2016-08-27 23:16:38 +0200

    m

---
+++ NetBeansProjects/Schroedinger/src/de/brenner/schroedinger/heapuebung/Basics.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.brenner.schroedinger.heapuebung;
+
+/**
+ *
+ * @author jonat
+ */
+public class Basics {
+       public static void main(String [] args){
+           int nenner = 2;
+           int zaehler = 20;
+           System.out.printf(format, args);
+       }
+}
