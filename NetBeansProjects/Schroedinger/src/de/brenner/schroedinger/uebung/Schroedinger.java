01c28c51c785734d81b9df5959f06a914d097d86
jonbert <jonbert@jonbert-Aspire-V5-573G>
2016-11-20 18:16:28 +0100

    committest

--- NetBeansProjects/Schroedinger/src/de/brenner/schroedinger/uebung/Schroedinger.java
+++ NetBeansProjects/Schroedinger/src/de/brenner/schroedinger/uebung/Schroedinger.java
@@ -15,7 +15,7 @@ public class Schroedinger {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-     System.out.println("Hello World!");
+     System.out.println("Schroedinger says World!");
     }
     
 }
